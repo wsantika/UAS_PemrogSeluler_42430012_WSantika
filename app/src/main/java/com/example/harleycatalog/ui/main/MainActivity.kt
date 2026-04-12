ce5c1d5eaf60171c0ff09083681cdf4556a76e65
wsantika <wahyuputra7396@gmail.com>
2026-04-12 13:58:13 +0800

    Implement MotorAdapter and set up RecyclerView in MainActivity

--- app/src/main/java/com/example/harleycatalog/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/harleycatalog/ui/main/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.example.harleycatalog.ui.main
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.harleycatalog.utils.MotorUtils
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.harleycatalog.R
+import com.example.harleycatalog.adapter.MotorAdapter
+import com.example.harleycatalog.utils.MotorUtils
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvMotor: RecyclerView
+    private lateinit var adapter: MotorAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
+        rvMotor = findViewById(R.id.rvMotor)
+
+        val motorList = MotorUtils.getMotorList()
+
+        adapter = MotorAdapter(motorList)
+
+        rvMotor.layoutManager = LinearLayoutManager(this)
+        rvMotor.adapter = adapter
     }
 }
